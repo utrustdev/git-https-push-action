9b458093a3610ecf0dedb913e90120fe8d416040
nunoapfernandes <nunofernandes313@gmail.com>
2022-07-28 17:41:32 +0100

    Force push to heroku repo

--- entrypoint.sh
+++ entrypoint.sh
@@ -20,5 +20,4 @@ fi
 
 git config --global --add safe.directory /github/workspace
 git remote add $REMOTE $REMOTE_URL
-git pull
-git push $(join_by " " $EXTRA_ARGS) $REMOTE HEAD:master
+git push --force $(join_by " " $EXTRA_ARGS) $REMOTE HEAD:master
